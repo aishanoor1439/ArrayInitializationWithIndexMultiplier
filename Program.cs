d648a4274aefbfbad9e56a742e24116190a7e773
aishanoor1439 <aisha.noor1439@gmail.com>
2025-01-21 09:28:20 +0100

    Final Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayInitializationWithIndexMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i * 5;
+            }
+
+            Console.WriteLine("Index\tValue");
+            Console.WriteLine("----------------");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}\t{array[i]}");
+            }
+
+        }
+    }
+}
